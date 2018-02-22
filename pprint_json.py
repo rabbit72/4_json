09728014a80bf30fe99304740f69c0098b01440f
Daniil Shadrin <rabbit72rus@gmail.com>
2018-02-22 16:08:49 +0300

    fix variables names again

--- pprint_json.py
+++ pprint_json.py
@@ -4,15 +4,15 @@ import sys
 
 def load_data(path):
     with open(path) as f:
-        data = json.loads(f.read())
-    return data
+        obj_json = json.loads(f.read())
+    return obj_json
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(obj_json):
+    print(json.dumps(obj_json, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    filepath = sys.argv[1]
-    data = load_data(filepath)
-    pretty_print_json(data)
+    file_path = sys.argv[1]
+    obj_json = load_data(file_path)
+    pretty_print_json(obj_json)
