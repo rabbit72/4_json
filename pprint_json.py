410cc72f92eed7e02d55ae7feaaa902e3a02e7bc
dany <rabbit72rus@gmail.com>
2018-02-23 01:24:21 +0300

    fix variables names and add processing exceptions

--- pprint_json.py
+++ pprint_json.py
@@ -3,16 +3,27 @@ import sys
 
 
 def load_data(path):
-    with open(path) as f:
-        obj_json = json.loads(f.read())
-    return obj_json
+    with open(path) as file_json:
+        deserialized_json = json.loads(file_json.read())
+    return deserialized_json
 
 
-def pretty_print_json(obj_json):
-    print(json.dumps(obj_json, sort_keys=True, indent=4, ensure_ascii=False))
+def pretty_print_json(deserialized_json):
+    print(json.dumps(deserialized_json, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    obj_json = load_data(file_path)
-    pretty_print_json(obj_json)
+    try:
+        file_path = sys.argv[1]
+    except IndexError:
+        print('No path to file. Try again entering the path.')
+        sys.exit()
+    try:
+        deserialized_json = load_data(file_path)
+    except FileNotFoundError:
+        print('File on entered path was not found. Check path to file.')
+        sys.exit()
+    except json.decoder.JSONDecodeError:
+        print('File on the entered path not JSON format')
+        sys.exit()
+    pretty_print_json(deserialized_json)
