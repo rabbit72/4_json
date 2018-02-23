cda0bbafc679244192ae502c6e1e3f3d3c31394b
Daniil Shadrin <rabbit72rus@gmail.com>
2018-02-23 11:17:04 +0300

    transfer sys.exit() and try/except of functions

--- pprint_json.py
+++ pprint_json.py
@@ -3,30 +3,25 @@ import sys
 
 
 def load_data(path):
-    try:
-        with open(path) as file_json:
-            try:
-                deserialized_json = json.loads(file_json.read())
-            except json.decoder.JSONDecodeError:
-                print('File on the entered path not JSON format')
-                return None
-    except FileNotFoundError:
-        print('File on the entered path was not found. Check path to file.')
-        return None
-    return deserialized_json
+    with open(path) as file_json:
+        deserialized_json = json.loads(file_json.read())
+        return deserialized_json
 
 
 def pretty_print_json(deserialized_json):
-    print(json.dumps(deserialized_json, sort_keys=True, indent=4, ensure_ascii=False))
+    print(json.dumps(deserialized_json, sort_keys=True,
+                     indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     try:
         file_path = sys.argv[1]
+        deserialized_json = load_data(file_path)
+        pretty_print_json(deserialized_json)
     except IndexError:
-        print('No path to file. Try again entering the path.')
-        sys.exit()
-    deserialized_json = load_data(file_path)
-    if deserialized_json is None:
-        sys.exit()
-    pretty_print_json(deserialized_json)
+        sys.exit('No path to file. Try again entering the path')
+    except FileNotFoundError:
+        sys.exit('File on the entered path was not found.'
+                 'Check path to file')
+    except json.decoder.JSONDecodeError:
+        sys.exit('File on the entered path not JSON format')
